a9b6ab54500e55d9c73052417dc37ec161f4df85
asmitaraysimpli <asmitaraysimpli@ip-172-31-72-165.ec2.internal>
2021-07-23 10:24:54 +0000

    Add project skeleton

---
+++ src/main/java/com/simplilearn/piperchart/PiperchartApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.piperchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiperchartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiperchartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/piperchart/PiperchartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.piperchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiperchartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
